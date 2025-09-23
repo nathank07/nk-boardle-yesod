aa61045900be4bb8ede316a047a7025691194d3d
nathank07 <nathank7256@gmail.com>
2025-09-23 00:55:53 -0500

    Added wordle logic

---
+++ src/Boardle/Boardle.hs
@@ -0,0 +1,54 @@
+module Boardle.Boardle
+    ( getSANMoves
+    , getSANMoves'
+    ) where
+
+import Game.Chess
+import Game.Chess.SAN
+import Control.Monad (foldM)
+
+data Guess = Green
+           | Yellow
+           | MaybeYellow String
+           | Gray
+           | Unknown String
+    deriving (Eq, Show)
+
+type FEN = String
+type UCI = String
+type SAN = String
+type Answer = String
+
+getGuesses' :: [Guess] -> [Answer] -> [Guess]
+getGuesses' = zipWith f
+    where 
+        f (Unknown a) b 
+         | a == b = Green
+         | otherwise = Gray
+        f (MaybeYellow a) b 
+         | a == b = Green
+         | otherwise = Yellow
+        f _ _ = Gray 
+
+getGuesses'' :: [Guess] -> [Answer] -> [Guess]
+getGuesses'' = foldl replaceFirstYellow 
+    where 
+        replaceFirstYellow [] _ = []
+        replaceFirstYellow ((Unknown g):gs) a 
+         | a == g = MaybeYellow g : gs
+         | otherwise = (Unknown g) : replaceFirstYellow gs a
+        replaceFirstYellow (g:gs) a = g : replaceFirstYellow gs a
+
+getGuesses :: [Guess] -> [Answer] -> [Guess]
+getGuesses guesses answers = getGuesses'' (getGuesses' guesses answers) answers
+
+getSANMoves :: Position -> [FEN] -> Maybe [SAN]
+getSANMoves pos uciMoves = fmap (reverse . snd) $ foldM step (pos, []) uciMoves
+    where
+        step (currPos, sanMoves) uci =
+            (\p -> (unsafeDoPly currPos p, toSAN currPos p : sanMoves))
+                <$> fromUCI currPos uci
+
+getSANMoves' :: FEN -> [UCI] -> Maybe [SAN]
+getSANMoves' fenStr uciMoves = (fromFEN fenStr) >>= (\pos -> getSANMoves pos uciMoves)
+
--- src/Database/LoadCsv.hs
+++ src/Database/LoadCsv.hs
@@ -7,17 +7,7 @@ module Database.LoadCsv
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
-import Game.Chess
-import Game.Chess.SAN
-import Control.Monad (foldM)
-
-getSANMoves :: Position -> [String] -> Maybe [String]
-getSANMoves pos uciMoves = fmap (reverse . snd) $ foldM step (pos, []) uciMoves
-    where
-        step (currPos, sanMoves) uci =
-            (\p -> (unsafeDoPly currPos p, toSAN currPos p : sanMoves))
-                <$> fromUCI currPos uci
-
+import Boardle.Boardle (getSANMoves')
 
 data PuzzleRecord = PuzzleRecord
     { puzzleId :: !String
@@ -45,9 +35,9 @@ loadCsv = do
     case decodeByNameWithP valueParse defaultDecodeOptions csvData of
         Left err -> putStrLn err
         Right (_, v) -> V.forM_ v $ \ p ->
-            case fromFEN (fen p) of
-                Just pos -> case getSANMoves pos (words (moves p)) of
-                    Just sanMoves -> putStr ""
-                    Nothing -> putStrLn "Error converting moves"
-                Nothing -> putStrLn "Error parsing FEN"
+            case getSANMoves' (fen p) (words $ moves p) of
+                (Just x) -> case length x of
+                    6 -> putStrLn $ unwords x
+                    _ -> putStr ""
+                _ -> putStrLn "Fail"
     putStrLn "Done"
